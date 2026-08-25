3ab76adc146fc41bea429fedb578709e72c5b6b1
thinkerhour <thinkerhour@gmail.com>
2026-08-25 21:23:04 +0700

    Fix: vercel deploy and artisan serve

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,4 +2,42 @@
 
 namespace App\Providers;
 
-// Service Provider Aplikasi
+use Illuminate\Foundation\Console\ServeCommand;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        if (class_exists(ServeCommand::class)) {
+            $allKeys = array_unique(array_merge(
+                ServeCommand::$passthroughVariables,
+                array_keys($_SERVER),
+                array_keys($_ENV),
+                array_keys(getenv()),
+                [
+                    'SystemRoot', 'SYSTEMROOT', 'systemroot',
+                    'Path', 'PATH', 'path',
+                    'windir', 'WINDIR', 'WinDir',
+                    'TEMP', 'TMP', 'Temp', 'Tmp',
+                    'LOCALAPPDATA', 'APPDATA', 'LocalAppData', 'AppData',
+                    'USERPROFILE', 'HOMEDRIVE', 'HOMEPATH', 'UserProfile',
+                    'ProgramData', 'ProgramFiles', 'CommonProgramFiles',
+                    'COMSPEC', 'PATHEXT', 'SystemDrive', 'ComSpec',
+                ]
+            ));
+            ServeCommand::$passthroughVariables = $allKeys;
+        }
+    }
+}
